822937a70b5fb47cc40d9e1fbfbdf15cf80d0999
azinasili <azinasili@gmail.com>
2014-04-23 22:21:41 -0400

    began rewrite and restructure of website

--- website/Gruntfile.js
+++ website/Gruntfile.js
@@ -1,299 +1,294 @@
-/*!
- * Azin Asili Gruntfile
- * http://azinasili.com
- *
- * Based off of Fireshell
- * http://getfireshell.com
- * @author Todd Motto
- */
-
 'use strict';
 
-/**
- * Livereload and connect variables
- */
-var LIVERELOAD_PORT = 35729;
-var lrSnippet = require('connect-livereload')({
-  port: LIVERELOAD_PORT
-});
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
-
-/**
- * Grunt module
- */
+// Grunt module
 module.exports = function (grunt) {
 
-  /**
-   * Dynamically load npm tasks
-   */
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // load all grunt tasks matching the `grunt-*` pattern
+  // https://github.com/sindresorhus/load-grunt-tasks
+  require('load-grunt-tasks')(grunt);
 
-  /**
-   * FireShell Grunt config
-   */
+  // Project configuration
   grunt.initConfig({
-
     pkg: grunt.file.readJSON('package.json'),
 
-    /**
-     * Set project info
-     */
+    // Set project variables
+    // Make file name changes here
     project: {
-      src: 'src',
-      app: 'app',
-      assets: '<%= project.app %>/assets',
-      css: [
-        '<%= project.src %>/scss/style.scss'
-      ],
-      js: [
-        '<%= project.src %>/js/*.js'
-      ]
+      src    : 'src',
+      app    : 'app',
+      deploy : 'deploy',
+      srcjs  : '<%= project.src %>/js/{,*/}*.js',
+      srccss : '<%= project.src %>/scss/spe.scss',
+      appjs  : '<%= project.app %>/js/spe.min.js',
+      appcss : '<%= project.app %>/css/spe.min.css',
+      vendor : '<%= project.app %>/js/vendor/'
     },
 
-    /**
-     * Project banner
-     * Dynamically appended to CSS/JS files
-     * Inherits text from package.json
-     */
+    // Project banner
+    // Dynamically appended to CSS/JS files
+    // Inherits text from package.json
     tag: {
       banner: '/*!\n' +
               ' * <%= pkg.name %>\n' +
               ' * <%= pkg.title %>\n' +
+              ' * <%= pkg.description %>\n' +
               ' * <%= pkg.url %>\n' +
-              ' * @author <%= pkg.author %>\n' +
               ' * @version <%= pkg.version %>\n' +
-              ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+              ' * Copyright <%= pkg.copyright %>.\n' +
               ' */\n'
     },
 
-    /**
-     * Connect port/livereload
-     * https://github.com/gruntjs/grunt-contrib-connect
-     * Starts a local webserver and injects
-     * livereload snippet
-     */
-    connect: {
-      options: {
-        port: 9000,
-        hostname: '*'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [lrSnippet, mountFolder(connect, 'app')];
-          }
-        }
-      }
-    },
-
-    /**
-     * Clean files and folders
-     * https://github.com/gruntjs/grunt-contrib-clean
-     * Remove generated files for clean deploy
-     */
-    clean: {
-      dist: [
-        '<%= project.assets %>/css/style.unprefixed.css',
-        '<%= project.assets %>/css/style.prefixed.css'
-      ]
-    },
-
-    /**
-     * Concatenate JavaScript files
-     * https://github.com/gruntjs/grunt-contrib-concat
-     * Imports all .js files and appends project banner
-     */
-    concat: {
-      dev: {
-        files: {
-          '<%= project.assets %>/js/scripts.min.js': '<%= project.js %>'
-        }
-      },
-      options: {
-        stripBanners: true,
-        nonull: true,
-        banner: '<%= tag.banner %>'
-      }
-    },
-
-    /**
-     * Uglify (minify) JavaScript files
-     * https://github.com/gruntjs/grunt-contrib-uglify
-     * Compresses and minifies all JavaScript files into one
-     */
-    uglify: {
-      options: {
-        banner: '<%= tag.banner %>'
-      },
-      dist: {
-        files: {
-          '<%= project.assets %>/js/scripts.min.js': '<%= project.js %>'
-        }
-      }
-    },
-
-    /**
-     * Compile Sass/SCSS files
-     * https://github.com/gruntjs/grunt-contrib-sass
-     * Compiles all Sass/SCSS files and appends project banner
-     */
+    // Compile Sass/SCSS files
+    // https://github.com/gruntjs/grunt-contrib-sass
     sass: {
       dev: {
         options: {
-          style: 'expanded',
-          banner: '<%= tag.banner %>'
+          cacheLocation : '_tmp',
+          lineNumbers   : 'true',
+          precision     : 10,
+          style         : 'expanded'
         },
         files: {
-          '<%= project.assets %>/css/style.unprefixed.css': '<%= project.css %>'
+          '<%= project.appcss %>' : '<%= project.srccss %>'
         }
       },
       dist: {
         options: {
-          style: 'expanded'
+          banner        : '<%= tag.banner %>',
+          cacheLocation : '_tmp',
+          precision     : 10,
+          style         : 'compressed'
         },
         files: {
-          '<%= project.assets %>/css/style.unprefixed.css': '<%= project.css %>'
+          '<%= project.appcss %>' : '<%= project.srccss %>'
         }
       }
     },
 
-    /**
-     * Autoprefixer
-     * Adds vendor prefixes automatically
-     * https://github.com/nDmitry/grunt-autoprefixer
-     */
+    // Adds vendor prefixes automatically
+    // https://github.com/nDmitry/grunt-autoprefixer
     autoprefixer: {
       options: {
         browsers: [
           'last 2 version',
           'safari 6',
+          'ie 8',
           'ie 9',
           'opera 12.1',
           'ios 6',
           'android 4'
         ]
       },
-      dev: {
-        files: {
-          '<%= project.assets %>/css/style.min.css': ['<%= project.assets %>/css/style.unprefixed.css']
-        }
-      },
       dist: {
-        files: {
-          '<%= project.assets %>/css/style.prefixed.css': ['<%= project.assets %>/css/style.unprefixed.css']
-        }
+        src : '<%= project.appcss %>'
       }
     },
 
-    /**
-     * CSSMin
-     * CSS minification
-     * https://github.com/gruntjs/grunt-contrib-cssmin
-     */
-    cssmin: {
-      dev: {
-        options: {
-          banner: '<%= tag.banner %>'
-        },
-        files: {
-          '<%= project.assets %>/css/style.min.css': [
-            '<%= project.assets %>/css/style.unprefixed.css'
-          ]
-        }
-      },
-      dist: {
-        options: {
-          banner: '<%= tag.banner %>'
-        },
-        files: {
-          '<%= project.assets %>/css/style.min.css': [
-            '<%= project.assets %>/css/style.prefixed.css'
-          ]
-        }
-      }
-    },
-
-    /**
-     * Build bower components
-     * https://github.com/yatskevich/grunt-bower-task
-     */
+    // Build bower components
+    // https://github.com/yatskevich/grunt-bower-task
     bower: {
+      install: {
+        options: {
+          targetDir : '<%= project.vendor %>',
+          cleanup   : true,
+          layout    : 'byType'
+        }
+      }
+    },
+
+    // JSHint
+    // https://github.com/gruntjs/grunt-contrib-jshint
+    // Manage the options inside .jshintrc file
+    jshint: {
+      options: {
+        jshintrc : '.jshintrc'
+      },
+      files: '<%= project.srcjs %>/*.js'
+    },
+
+    // Concatenate JavaScript files
+    // https://github.com/gruntjs/grunt-contrib-concat
+    concat: {
+      options: {
+        banner       : '<%= tag.banner %>',
+        stripBanners : true,
+        nonull       : true,
+      },
       dev: {
-        dest: '<%= project.assets %>/components/'
+        src  : '<%= project.srcjs %>',
+        dest : '<%= project.appjs %>'
+      }
+    },
+
+    // Minify and Uglifies JS
+    // https://github.com/gruntjs/grunt-contrib-uglify
+    uglify: {
+      options: {
+        mangle           : true,
+        compress         : true,
+        report           : 'min',
+        preserveComments : 'some'
+      },
+      dev: {
+        files: {
+          '<%= project.vendor %>/jquery.min.js'    : '<%= project.vendor %>/jquery/jquery.js',
+          '<%= project.vendor %>/modernizr.min.js' : '<%= project.vendor %>/modernizr/modernizr.js'
+        }
       },
       dist: {
-        dest: '<%= project.assets %>/components/'
+        files: {
+          '<%= project.appjs %>' : '<%= project.appjs %>'
+        }
       }
     },
 
-    /**
-     * Opens the web server in the browser
-     * https://github.com/jsoverson/grunt-open
-     */
-    open: {
-      server: {
-        path: 'http://localhost:<%= connect.options.port %>'
+    // Compress images
+    // https://github.com/gruntjs/grunt-contrib-imagemin
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand : true,
+          cwd    : '<%= project.app %>/img',
+          src    : ['**/*.{png,jpg,gif}'],
+          dest   : '<%= project.deploy %>/img'
+        }]
       }
     },
 
-    /**
-     * Runs tasks against changed watched files
-     * https://github.com/gruntjs/grunt-contrib-watch
-     * Watching development files and run concat/compile tasks
-     * Livereload the browser once complete
-     */
+    // Clean directories of unneeded folders and files
+    // https://github.com/gruntjs/grunt-contrib-clean
+    clean: {
+      dev: [
+        '<%= project.vendor %>/jquery',
+        '<%= project.vendor %>/modernizr'
+      ],
+      dist: [
+        '_tmp'
+      ]
+    },
+
+    // Copy images into app folder
+    // Copy app directory into a deploy directory
+    // https://github.com/gruntjs/grunt-contrib-copy
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          cwd: '<%= project.src %>/img',
+          src: '**',
+          dest: '<%= project.app %>/img/'
+        }]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= project.app %>',
+          src: '**',
+          dest: '<%= project.deploy %>/'
+        }]
+      }
+    },
+
+    // Minify HTML
+    // https://github.com/gruntjs/grunt-contrib-htmlmin
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments            : true,
+          collapseWhitespace        : true,
+          removeRedundantAttributes : true,
+          removeEmptyAttributes     : true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= project.app %>',
+          src: '*.html',
+          dest: '<%= project.deploy %>'
+        }]
+      }
+    },
+
+    // Runs tasks against changed watched files
+    // https://github.com/gruntjs/grunt-contrib-watch
+    // Watching development files and run concat/compile tasks
     watch: {
       concat: {
-        files: '<%= project.src %>/js/{,*/}*.js',
-        tasks: ['concat:dev', 'jshint']
+        files : '<%= project.srcjs %>',
+        tasks : ['concat', 'jshint']
       },
       sass: {
-        files: '<%= project.src %>/scss/{,*/}*.{scss,sass}',
-        tasks: ['sass:dev', 'cssmin:dev', 'autoprefixer:dev']
+        files : '<%= project.src %>/scss/{,*/}*.{scss,sass}',
+        tasks : ['sass:dev', 'autoprefixer']
       },
-      livereload: {
-        options: {
-          livereload: LIVERELOAD_PORT
-        },
-        files: [
-          '<%= project.app %>/{,*/}*.html',
-          '<%= project.assets %>/css/*.css',
-          '<%= project.assets %>/js/{,*/}*.js',
-          '<%= project.assets %>/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
-    }
+      // livereload: {
+      //   options: {
+      //     livereload: '<%= connect.options.livereload %>'
+      //   },
+      //   files: [
+      //     '<%= project.app %>/{,*/}*.html',
+      //     '<%= project.appcss %>',
+      //     '<%= project.appjs %>',
+      //     '<%= project.app %>/img/{,*/}*'
+      //   ]
+      // }
+    },
+
+    // connect: {
+    //   options: {
+    //     port: 9000,
+    //     open: true,
+    //     livereload: 35729,
+    //     // Change this to '0.0.0.0' to access the server from outside
+    //     hostname: 'localhost'
+    //   },
+    //   livereload: {
+    //     options: {
+    //       middleware: function(connect) {
+    //         return [
+    //           connect.static('.tmp'),
+    //           connect().use('/bower_components', connect.static('./bower_components')),
+    //           connect.static(config.app)
+    //         ];
+    //       }
+    //     }
+    //   },
+    //   dist: {
+    //     options: {
+    //       base: '<%= config.dist %>',
+    //       livereload: false
+    //     }
+    //   }
+    // },
+
   });
 
-  /**
-   * Default task
-   * Run `grunt` on the command line
-   */
+  // Default task for development
+  // Run `grunt` on the command line
   grunt.registerTask('default', [
     'sass:dev',
-    'cssmin:dev',
-    'bower:dev',
-    'autoprefixer:dev',
-    'concat:dev',
-    'connect:livereload',
-    'open',
+    'autoprefixer',
+    'bower',
+    'jshint',
+    'concat',
+    'uglify:dev',
+    'clean:dev',
+    'copy:dev',
+    // 'connect',
     'watch'
   ]);
 
-  /**
-   * Build task
-   * Run `grunt build` on the command line
-   * Then compress all JS/CSS files
-   */
-  grunt.registerTask('build', [
+  // Build task for production
+  // Run `grunt build` on the command line
+  grunt.registerTask('deploy', [
     'sass:dist',
-    'bower:dist',
-    'autoprefixer:dist',
-    'cssmin:dist',
-    'clean:dist',
-    'uglify'
+    'bower',
+    'jshint',
+    'concat',
+    'uglify',
+    'clean',
+    'copy:dist',
+    'imagemin',
+    'htmlmin'
   ]);
 
 };
