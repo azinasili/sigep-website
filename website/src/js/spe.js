b54815c66056264dd50437b1c94452730978cc40
Azin Asili <azinasili@gmail.com>
2014-05-28 23:02:22 -0400

    did some things

--- website/src/js/spe.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-window.onload=function() {
-  var footerHeight = document.getElementById('footer').offsetHeight;
-  document.getElementById('main').setAttribute('style', 'margin-bottom: ' + footerHeight + 'px;');
-};
