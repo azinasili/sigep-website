6dbf700a876b8aee4e395635b3fe192661bf3fe6
Azin Asili <azinasili@gmail.com>
2014-08-04 00:02:48 -0400

    added btn to article and removed transform animation on hero text

--- website/src/js/hero-image-scroll.js
+++ website/src/js/hero-image-scroll.js
@@ -1,1 +1,10 @@
-'ise strict';
+// $(document).ready(function() {
+//   $(window).scroll(function(){
+//     $('#hero').each(function(r){
+//       var pos = $(this).offset().top;
+//       var scrolled = $(window).scrollTop();
+//       $('.hero-content').css('transform', 'translate(-50%, ' + (scrolled * 0.2) + 'px)');
+//       $('#hero img').css('transform', 'translate(0, ' + -(scrolled * 0.15) + 'px)');
+//     });
+//   });
+// });
